0c27ba2e6ee07ba0e3261ae43b562b2c5a8c1939
Nguyen Van Nam <namnguyen2142006@gmail.com>
2026-06-10 13:46:42 +0700

    COMPLETE DATA.JS

---
+++ EXAM2026_CaX/html/data/data.js
@@ -0,0 +1,37 @@
+const duLieuMau = [
+  {
+    id: 1,
+    ten: "Điện thoại iPhone 15",
+    loai: "Điện thoại",
+    gia: 24990000,
+    trangThai: "Còn hàng",
+  },
+  {
+    id: 2,
+    ten: "Máy tính bảng Samsung Galaxy Tab S9",
+    loai: "Máy tính bảng",
+    gia: 18500000,
+    trangThai: "Còn hàng",
+  },
+  {
+    id: 3,
+    ten: "Chuột Logitech MX Master 3S",
+    loai: "Phụ kiện",
+    gia: 2490000,
+    trangThai: "Hết hàng",
+  },
+  {
+    id: 4,
+    ten: "Máy tính xách tay MacBook Air M3",
+    loai: "Laptop",
+    gia: 31990000,
+    trangThai: "Còn hàng",
+  },
+  {
+    id: 5,
+    ten: "Tai nghe Sony WH-1000XM5",
+    loai: "Tai nghe",
+    gia: 7990000,
+    trangThai: "Còn hàng",
+  },
+];
